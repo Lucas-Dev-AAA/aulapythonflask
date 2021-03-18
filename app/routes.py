686fe7e79c971136fea0361d836978f540da2788
Lucas-Dev-AAA <lucasfelixm12@gmail.com>
2021-03-18 19:38:34 +0000

    Hello World

--- app/routes.py
+++ app/routes.py
@@ -7,3 +7,6 @@ from flask import render_template
 def index():
     user = {'username' : 'Lucas Félix'}
     return render_template("index.html", user=user)
+    posts = [
+        {'author'}
+    ]
